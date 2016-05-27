feacb7351bb8d6a9a6952f6e25080eaff75cf0a2
Abhishek Dasgupta <a10gupta@linux.vnet.ibm.com>
2016-05-27 19:00:12 +0530

    Hypervisor support for Mesos.

--- src/slave/containerizer/containerizer.cpp
+++ src/slave/containerizer/containerizer.cpp
@@ -40,6 +40,7 @@
 #include "slave/containerizer/external_containerizer.hpp"
 
 #include "slave/containerizer/mesos/containerizer.hpp"
+#include "slave/containerizer/hypervisor/containerizer.hpp"
 #include "slave/containerizer/mesos/launcher.hpp"
 #ifdef __linux__
 #include "slave/containerizer/mesos/linux_launcher.hpp"
@@ -291,6 +292,16 @@ Try<Containerizer*> Containerizer::create(
         DockerContainerizer::create(flags, fetcher, nvidia);
       if (containerizer.isError()) {
         return Error("Could not create DockerContainerizer: " +
+                     containerizer.error());
+      } else {
+        containerizers.push_back(containerizer.get());
+      }
+    } else if (type == "hypervisor") {
+      Try<HypervisorContainerizer*> containerizer =
+      HypervisorContainerizer::create(flags, fetcher);
+
+      if (containerizer.isError()) {
+        return Error("Could not create HypervisorContainerizer: " +
                      containerizer.error());
       } else {
         containerizers.push_back(containerizer.get());
